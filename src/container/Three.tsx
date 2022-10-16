bd6910f5a343e2756df6876deaba04e7a037f14c
Julia Undeutsch <JUndeutsch@gmx.at>
2022-10-16 20:20:29 +0200

    create reusable button

--- src/container/Three.tsx
+++ src/container/Three.tsx
@@ -1,11 +1,22 @@
+import Buttons from '../components/Buttons';
+
 const Three = () => {
+
+  const content = [
+    "Architecture styles",
+    "Tradeoffs analysis"
+  ]
+
   return (
-    <div className="container mx-auto bg-white rounded-xl h-64 shadow-lg">
+    <div className="container mx-auto bg-white rounded-xl min-h-64 shadow-lg">
       <p className="text-5xl font-extrabold py-4 text-center px-2">
         <span className="bg-clip-text text-transparent bg-gradient-to-r from-sky-400 to-indigo-500">
           Software Architecture
         </span>
       </p>
+      <div className="flex justify-around flex-wrap">
+        <Buttons content={content} />
+      </div>
     </div>
   );
 }
--- src/container/Two.tsx
+++ src/container/Two.tsx
@@ -1,11 +1,29 @@
+import Buttons from '../components/Buttons';
+
 const Two = () => {
+
+  const content = [
+    "Testing, e.g",
+    "Integration tests",
+    "E2E tests",
+    "Patterns to structure tests",
+    "Mocks and spies",
+    "Effectively measure test coverage",
+    "Tooling",
+    "Leverage your IDE",
+    "Language fundamentals"
+  ]
+
   return (
-    <div className="container mx-auto bg-white rounded-xl h-64 shadow-lg">
+    <div className="container mx-auto bg-white rounded-xl min-h-64 shadow-lg">
       <p className="text-5xl font-extrabold py-4 text-center px-2">
         <span className="bg-clip-text text-transparent bg-gradient-to-r from-green-400 to-cyan-500">
           Code Quality
         </span>
       </p>
+      <div className="flex justify-around flex-wrap">
+        <Buttons content={content} />
+      </div>
     </div>
   );
 }
